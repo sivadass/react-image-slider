8309f47423b4a68415deb43b3a9120ca233094fc
sivadass <nsivadass@gmail.com>
2017-08-05 21:41:43 +0530

    Added page header

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ class App extends React.Component{
   render(){
     return(
       <div className="wrapper">
-        <h1>React Image Slider</h1>
+        <header>
+          <h1>React Image Slider</h1>
+          <p>A minimal react image carousel component.</p>
+        </header>
         <Slider />
       </div>
     )
